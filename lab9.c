f2438a6c2e60118d8dd2055cce6beb0cef5e157d
Hyunseung Do <Steve@7c-d1-c3-92-eb-92.wireless.temple.edu>
2017-04-07 14:07:25 -0400

    this is difficult. find quotations by locating pointer location in file

--- lab9.c
+++ lab9.c
@@ -6,12 +6,50 @@
 
 //header files
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int findNextQuote(FILE *fPtr);
 
 int main(void){
-    FILE * textPtr; //this is the file pointer to the sherlock.txt
-    /* FILE * outPtr; //this is the file pointer to the output.txt */
+    FILE * fp; //this is the file pointer to the sherlock.txt
 
-    textPtr = fopen("sherlock.txt", "r+");
+    //gotta make sure the file is able to be opened first
+    if ((fp = fopen("sherlock.txt", "r+")) == NULL){ //the file pointer opens the file in update mode (read and write)
+        puts("Can't find the file!");       
+        exit(1);
+    } 
+    char sentence[1000]; //this is the char array that contains the quotation
+    
+    //finding the location of quotation marks
+    fscanf(fp, "%*[^\"]%c", sentence); //search until quotation mark
+    int pos = ftell(fp); //gets the location of it
+    fscanf(fp, "%*[^\"]%c", sentence); //search until another quotation mark
+    int pos2 = ftell(fp); //gets the location of it
+    int pos3 = findNextQuote(fp);
+    int pos4 = findNextQuote(fp);
+    printf("%d %d\n", pos, pos2); //testing the locations of quotation marks
+    printf("%d %d\n", pos3, pos4); //testing the locations of quotation marks
 
-    fclose(textPtr);
+    fseek(fp, pos, SEEK_SET);
+    fgets(sentence, pos2 - pos, fp);
+    printf("%s\n", sentence);
+    memset(sentence, 0, sizeof(sentence));
+
+    fseek(fp, pos3, SEEK_SET);
+    fgets(sentence, pos4 - pos3, fp);
+    printf("%s\n", sentence);
+    
+    /* //use memset to directly change the letters of the word */ 
+    /* memset(sentence, 'h', 7); */
+    /* printf("%s\n", sentence); //testing the effect of memset */
+
+    fclose(fp); //always close the pointer
+}
+
+int findNextQuote(FILE *fPtr){
+    FILE *ptr = fPtr;
+    char trash[2];
+    fscanf(ptr, "%*[^\"]%c", trash);
+    return ftell(ptr);
 }
