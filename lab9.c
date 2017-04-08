61139fc3c32b32db17a2445bf6eae57c7f918a1d
Steve Do <gustmd1210@gmail.com>
2017-04-08 17:31:30 -0400

    figured out how to get rid of carriage return characters and write to output

--- lab9.c
+++ lab9.c
@@ -9,44 +9,53 @@
 #include <stdlib.h>
 #include <string.h>
 
+char * cleanQuote(char quote[]); //replaces '\r' with '\n'
+
 int main(void){
-    FILE * fp; //this is the file pointer to the sherlock.txt
-    FILE * fp2;
+    FILE * input; //this is the file pointer to the sherlock.txt
+    FILE * output; //this is the file pointer to the new.txt, the output text with the Southie accent dialogues
 
     //gotta make sure the file is able to be opened first
-    if ((fp = fopen("sherlock.txt", "r+")) == NULL){ //the file pointer opens the file in update mode (read and write)
+    if ((input = fopen("sherlock.txt", "r+")) == NULL){ //the file pointer opens the file in update mode (read and write)
         puts("Can't find the file!");       
         exit(1);
     } 
     
-    fp2 = fopen("new.txt", "w+");
+    output = fopen("southie-sherlock.txt", "w+");
+    char sentence[5000]; //this is the char array that contains the quotation
 
-    char sentence[1000]; //this is the char array that contains the quotation
+    fscanf(input, "%*[^\"]%c", sentence);
+    int pos1 = ftell(input);
+    rewind(input);
 
-    fscanf(fp, "%*[^\"]%c", sentence);
-    int pos1 = ftell(fp);
-    printf("%s\n", sentence);
-    fscanf(fp, "%*[^\"]%c", sentence);
-    int pos2 = ftell(fp);
-    printf("%s\n", sentence);
-    printf("%d %d\n", pos1, pos2);
-     
-    fscanf(fp, "%*[^\"]%c", sentence);
-    int pos3 = ftell(fp);
-    printf("%s\n", sentence);
-    fscanf(fp, "%*[^\"]%c", sentence);
-    int pos4 = ftell(fp);
-    printf("%s\n", sentence);
-    printf("%d %d\n", pos3, pos4);
+    fprintf(output, "%s", cleanQuote(fgets(sentence, pos1, input)));
+    pos1 = ftell(input);
+    printf("%d\n", pos1);
 
+    
 
-    fclose(fp); //always close the pointer
-    fclose(fp2);
+    fclose(input); //always close the pointer
+    fclose(output);
 }
 
 /* 
- * %*[^\"]%c
+ * %*[^\"]%c <= to get the quotation mark in the beginning
+ * %*[^\"]\"%[^\"] <= to get the dialogue in the quotation without the quotation marks
  *
- * %*[^\"] reads from stream until \ is encountered. it stops right before
- * %c reads the actual \
+ * %*[^\"] reads from stream until " is encountered. it stops right before
+ * %c reads the actual "
  * */
+
+//the text file uses carriage-return character, '\r' to make new line
+//however, this creates interruption with printf
+//the function replaces '\r' with '\n' in given char array
+char * cleanQuote(char quote[]){
+    int i;
+    for (i = 0; i < 5000; i++){
+        if (quote[i] == '\r'){
+            quote[i] = '\n';
+        }
+    }
+
+    return quote;
+}
