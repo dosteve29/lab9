e577793755492d977c88b49c6c88761e27a80380
Steve Do <gustmd1210@gmail.com>
2017-04-12 01:04:38 -0400

    able to loop through the whole text and print them to new file

--- lab9.c
+++ lab9.c
@@ -10,41 +10,47 @@
 #include <string.h>
 
 char * cleanQuote(char quote[]); //replaces '\r' with '\n'
+void fixMultiParagraph();
 
 int main(void){
     FILE * input; //this is the file pointer to the sherlock.txt
-    FILE * output; //this is the file pointer to the new.txt, the output text with the Southie accent dialogues
+    FILE * output; //this is the file pointer to the southie-sherlock.txt
+    int tracker;
+    int startPos = 0; //starting position of quotation
+    int endPos = 0; //ending position of quotation
 
     //gotta make sure the file is able to be opened first
     if ((input = fopen("sherlock.txt", "r+")) == NULL){ //the file pointer opens the file in update mode (read and write)
         puts("Can't find the file!");       
         exit(1);
     } 
-    
+
+    //the output is created if not existing and overwritten every time
     output = fopen("southie-sherlock.txt", "w+");
     char sentence[5000]; //this is the char array that contains the quotation
+    char non[5000];
 
-    fscanf(input, "%*[^\"]%c", sentence);
-    int pos1 = ftell(input);
-    rewind(input);
+    //this is required to get the first part of non quotation
+    fscanf(input, "%[^\"]", non);
+    fprintf(output, "%s", cleanQuote(non));
 
-    fprintf(output, "%s", cleanQuote(fgets(sentence, pos1, input)));
-    pos1 = ftell(input);
-    printf("%d\n", pos1);
+    //this will loop through the whole text
+    //while printing back and forth between
+    //quote and non-quote
+    while (!feof(input)){
+        fscanf(input, "\"%[^\"]\"", sentence);
+        printf("\"%s\"", cleanQuote(sentence));
+        fprintf(output, "\"%s\"", cleanQuote(sentence));
 
-    
+        fscanf(input, "%[^\"]", non);
+        printf("%s", cleanQuote(non));
+        fprintf(output, "%s", cleanQuote(non));
+    }
 
     fclose(input); //always close the pointer
     fclose(output);
 }
 
-/* 
- * %*[^\"]%c <= to get the quotation mark in the beginning
- * %*[^\"]\"%[^\"] <= to get the dialogue in the quotation without the quotation marks
- *
- * %*[^\"] reads from stream until " is encountered. it stops right before
- * %c reads the actual "
- * */
 
 //the text file uses carriage-return character, '\r' to make new line
 //however, this creates interruption with printf
@@ -58,4 +64,8 @@ char * cleanQuote(char quote[]){
     }
 
     return quote;
+}
+
+void fixMultiParagraph(){
+
 }
